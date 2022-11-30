3dd9556cf69888a07e3e09a428c7c246497a7451
skdding3 <skdding3@gmail.com>
2022-11-30 11:15:28 +0900

    next start

---
+++ .next/static/bK9MScw_2UlAZud8vG4Pb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-93f6cf76e764e6d4.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
